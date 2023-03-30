f10318a4040fd17fb3fb559b41867f8834184a95
kovalchukim <kovalchuk.kim@gmail.com>
2023-03-30 22:18:10 +0300

    authorization and login to firebase by googleSignIn

---
+++ src/comonents/forms/Button/index.js
@@ -0,0 +1,15 @@
+import './styles.scss'
+
+const SingIn = props => {
+  return (
+    <div className="signin">
+      <div className="wrap">
+        <h2>
+          LogIn
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export default SingIn;
